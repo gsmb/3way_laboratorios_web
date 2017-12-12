d51528d7aa5f66189fda0130c6bccb2e50df47dc
romerohenrique <romerohenrique08@gmail.com>
2017-12-12 19:43:41 -0200

    levantando o projeto livraria

--- Livraria/src/dao/LivroDao.java
+++ Livraria/src/dao/LivroDao.java
@@ -1,9 +1,11 @@
 package dao;
 
-import java.awt.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.naming.spi.DirStateFactory.Result;
@@ -18,6 +20,8 @@ public class LivroDao {
 	private static final String OBTER_POR_ID_SQL = "SELECT AUTOR, TITULO, COD_LIVRO, IMAGEM, PRECO, DESCRICAO FROM ESTOQUE WHERE COD_LIVRO =?";
 	
 	private static final String CONSULTA_SQL ="SELECT COD_LIVROTITULO, COD_LIVRO, IMAGEM, PRECO, DESCRICAO FROM ESTOQUE WHERE TITULO LIKE ?";
+
+	private static final String CONSULTAR_SQL = null;
 	
 	public Livro consultar(int codigo){
 		Livro livro = null;
@@ -27,13 +31,40 @@ public class LivroDao {
 			if(resultado.next()){
 				livro = new Livro();
 				livro.setAutor(resultado.getString("AUTOR"));
+				livro.setCodigo(resultado.getInt("COD_LIVRO"));
+				livro.setImagem(resultado.getString("IMAGEM"));
+				livro.setPreco(resultado.getDouble("PRECO"));
+				livro.setTitulo(resultado.getString("TITULO"));
+				livro.setDescricao(resultado.getNString("DESCRICAO"));
+				
+				resultado.close();
 			}
-		} catch (Exception e) {
-			// TODO: handle exception
+		} catch (SQLException e) {
+
+			LOG.severe(e.toString());
 		}
 				
 		return livro;
 		
 	}
-	public List<Livro>co
+	public List<Livro> consultar(String titulo){
+		java.util.List<Livro> lista = new ArrayList<Livro>();
+		try (Connection conexao = FabricaConexao.getConnection(); PreparedStatement consulta = conexao.prepareStatement(CONSULTAR_SQL);){
+			consulta.setString(1, "%" + titulo.toUpperCase()+"%");
+			ResultSet resultado = consulta.executeQuery();
+			
+			while (resultado.next()){
+				Livro livro = new Livro();
+				
+				
+				lista.add(livro);
+			}
+			resultado.close();
+		} catch (SQLException e) {
+			LOG.severe(e.toString());
+		}
+		return lista;
+	}
+	
+	
 }
