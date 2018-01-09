4c9f9107082bb050a3c751e5fd74b72bf9eaf3eb
romerohenrique <romerohenrique08@gmail.com>
2018-01-09 21:33:20 -0200

    #1 msg: adicionando o ambiente

---
+++ workload_livraria/src/main/java/controler/LivroController.java
@@ -0,0 +1,32 @@
+package controler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LivroController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.sendRedirect("Inicio.html");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		super.doPost(req, resp);
+	}
+
+	
+}
