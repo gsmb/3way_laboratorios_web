b9f1908affc46f520b6bd52eeef77eaf75a1d111
romerohenrique <romerohenrique08@gmail.com>
2018-01-11 22:00:20 -0200

    #3 msg : testando update

---
+++ workload_livraria/src/main/java/data/DaoLivro.java
@@ -0,0 +1,28 @@
+package data;
+
+import model.Livro;
+
+public  class DaoLivro implements Dao<Livro> {
+
+	public Livro criar(Livro modelo) {
+		
+		return null;
+	}
+
+	public Livro recuperar(int id) {
+		
+		return null;
+	}
+
+	public Livro alterar(Livro modelo) {
+	
+		return null;
+	}
+
+	public Livro excluir(int id) {
+		
+		return "";
+	}
+
+	
+}
