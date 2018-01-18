98478b6797106341c1827fc6553f9646b38e3b5b
romerohenrique <romerohenrique08@gmail.com>
2018-01-18 20:52:48 -0200

    #5 msg: implementado a class LivroController

--- workload_livraria/src/main/java/controler/LivroController.java
+++ workload_livraria/src/main/java/controler/LivroController.java
@@ -1,13 +1,20 @@
 package controler;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import data.model.Livro;
+import service.LivroService;
+
 public class LivroController extends HttpServlet {
+	public static final String LISTA_LIVROS = "lista_livros";
 
 	/**
 	 * 
@@ -17,29 +24,69 @@ public class LivroController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
-		resp.sendRedirect("Inicio.html");
+		String cmdParam = req.getParameter("cmd");
+		switch (cmdParam) {
+		case LISTA_LIVROS:
+			listarLivros(req, resp);
+
+			break;
+
+		default:
+			resp.getWriter().println("Erro: comando inválido");
+			break;
+		}
+
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		
-		super.doPost(req, resp);
+	private void listarLivros(HttpServletRequest req, HttpServletResponse resp) {
+		LivroService livroService = new LivroService();
+		try {
+			List<Livro> livros = livroService.listLivros();
+			req.setAttribute("livros", livros);
+
+			RequestDispatcher rd = req
+					.getRequestDispatcher("/pages/lista_livros.jsp");
+			rd.forward(req, resp);
+		} catch (ServletException e) {
+			e.printStackTrace();
+			printMesnsagemErro(
+					"Erro ao tentar buuscar livro: " + e.getMessage(), resp);
+		} catch (ServletException e) {
+			e.printStackTrace();
+			printMesnsagemErro("Erro de Servlet: " + resp);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			printMesnsagemErro("Erro de IO: " + resp);
+
+		}
 	}
 
-	public String listarLivros(){
-		
-		return "livros";
+	@SuppressWarnings("unused")
+	private void printMensagemErro(String msg, HttpServletResponse resp) {
+		try {
+			resp.sendError(500, msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public void mostrarPaginaCadastroLivro(){
-		
-	}
-	
-	public void cadastroLivro(){
-		
-		
-	}
+	/**
+	 * @Override protected void doPost(HttpServletRequest req,
+	 *           HttpServletResponse resp) throws ServletException, IOException
+	 *           {
+	 * 
+	 *           super.doPost(req, resp); }
+	 * 
+	 *           public String listarLivros() {
+	 * 
+	 *           return "livros"; }
+	 * 
+	 *           public void mostrarPaginaCadastroLivro() {
+	 * 
+	 *           }
+	 * 
+	 *           public void cadastroLivro() {
+	 * 
+	 *           }
+	 **/
 }
-
